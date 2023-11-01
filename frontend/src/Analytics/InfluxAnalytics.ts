ebeb2ccd95307b07ff2f4073d1486f259d6d2511
rballinger <rballinger@topcon.com>
2023-11-01 13:43:21 +1000

    ..

--- frontend/src/Analytics/InfluxAnalytics.ts
+++ frontend/src/Analytics/InfluxAnalytics.ts
@@ -2,12 +2,20 @@ import { InfluxDB, Point, HttpError, WriteApi } from '@influxdata/influxdb-clien
 import Analytics from './Analytics';
 import { Duration } from 'moment';
 
+const getOrg = (): string => {
+    if (process.env.NODE_ENV !== 'production') return 'f30489c658c321d7'
+    return 'creativeilk'
+}
+
 const getClient = (): InfluxDB => {
     if (process.env.NODE_ENV !== 'production') return new InfluxDB({
         url: 'https://influx.babymonitor.local:8443',
         token: '1iA4Zsc1XAAw9vZprfHAnojjkPLE_9Ghm9mWH0yTEqgUbRBM3jc--JxIz0_3y-brA-fha3cJ2YqTa0maVRaSeQ==',
     })
-    throw new Error('Influx host not set');
+    return new InfluxDB({
+        url: 'https://influx.babymonitor.creativeilk.com',
+        token: 'QLO7fgbno91AETkt_pUTui4y_IA7vMx8Fd8O0D4y5w1RLhTZ58sb0wRXDw_Q4jjkJiBngoFvIWszM3UdbLu_Yw==',
+    })
 }
 
 class InfluxAnalytics implements Analytics {
@@ -15,7 +23,7 @@ class InfluxAnalytics implements Analytics {
 
     constructor(client_id: string) {
         console.log('Initializing Influx analytics');
-        const org = 'f30489c658c321d7';
+        const org = getOrg();
         const bucket = 'app';
         this.writer = getClient().getWriteApi(org, bucket, 'ms');
         this.writer.useDefaultTags({
