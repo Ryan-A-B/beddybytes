0fa8ede68a9fd895b02f4cd32e7e77aa4523dbe4
rballinger <rballinger@topcon.com>
2023-11-01 14:47:03 +1000

    ..

--- frontend/src/Instructions/YoutubeVideo/index.tsx
+++ frontend/src/Instructions/YoutubeVideo/index.tsx
@@ -13,12 +13,13 @@ const IconStyle: React.CSSProperties = {
 
 const YoutubeVideo: React.FunctionComponent<Props> = ({ src, title, thumbnail }) => {
     const [show, setShow] = React.useState(false)
-    const onMouseOver = React.useCallback(() => {
+    const switchToActualVideo = React.useCallback(() => {
         setShow(true)
     }, [])
     if (!show) return (
         <div
-            onMouseOver={onMouseOver}
+            onMouseOver={switchToActualVideo}
+            onTouchStart={switchToActualVideo}
             className="w-100 h-100 bg-dark"
         >
             <img
@@ -39,17 +40,9 @@ const YoutubeVideo: React.FunctionComponent<Props> = ({ src, title, thumbnail })
             src={src}
             title={title}
             className="w-100 h-100"
+            allowFullScreen
         />
     )
 }
 
 export default YoutubeVideo;
-
-{/* <iframe
-            src="https://www.youtube.com/embed/AuUb7thmu80?si=IEg92DaB-DnvrZof"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            className="w-100 h-100"
-        ></iframe> */}
