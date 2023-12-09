6bd4979fbddf0f671b595c5491817bf5d567d0db
rballinger <rballinger@topcon.com>
2023-12-09 15:40:43 +1000

    ..

--- frontend/src/instances/influx.ts
+++ frontend/src/instances/influx.ts
@@ -8,7 +8,7 @@ const getOrg = (): string => {
 const getClient = (): InfluxDB => {
     if (process.env.NODE_ENV !== 'production') return new InfluxDB({
         url: 'https://influx.babymonitor.local:8443',
-        token: '1iA4Zsc1XAAw9vZprfHAnojjkPLE_9Ghm9mWH0yTEqgUbRBM3jc--JxIz0_3y-brA-fha3cJ2YqTa0maVRaSeQ==',
+        token: 'vi3x4QTRweVCsA02Koqk1v1nhZ4b0C9ExZqz0C8ZFNrPs5npCGOt0KXkGY5L7aBXZ401K10Mbhc2yHABFH6Ljg==',
     })
     return new InfluxDB({
         url: 'https://influx.babymonitor.creativeilk.com',
