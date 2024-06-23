20fe20670ace045fcaa21fa857128ae90646bde8
rballinger <rballinger@topcon.com>
2024-06-24 09:36:21 +1000

    adds REACT_APP_BUILD_ env vars to docker container

--- scripts/frontend/build.sh
+++ scripts/frontend/build.sh
@@ -4,6 +4,8 @@ set -ex
 workdir=$(pwd)/frontend
 
 docker run --rm -it \
-    -v $workdir:$workdir \
+    -e REACT_APP_BUILD_TIMESTAMP=$(date +"%Y-%m-%dT%H:%M:%S%z") \
+    -e REACT_APP_BUILD_HASH=$(git rev-parse HEAD) \
+    -v $(pwd):$(pwd) \
     -w $workdir \
     node:18 yarn build
