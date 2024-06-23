9e6ea6c0204136d241854a833df910bb1b5de6c4
rballinger <rballinger@topcon.com>
2024-06-24 09:44:03 +1000

    ..

--- frontend/src/components/Footer/build_info.tsx
+++ frontend/src/components/Footer/build_info.tsx
@@ -1,14 +1,14 @@
 import { v4 as uuid } from 'uuid';
 
-const get_meta_content_or_default = (name: string, default_value: string): string => {
+const get_meta_content_or_default = (name: string): string => {
     const meta_element = document.querySelector(`meta[name="${name}"]`)
     if (meta_element === null)
-        return default_value;
+        throw new Error(`Meta element with name "${name}" not found`);
     const meta_element_content = meta_element.getAttribute('content');
     if (meta_element_content === null)
-        return default_value;
+        throw new Error(`Meta element with name "${name}" has no content`);
     return meta_element_content;
 }
 
-export const build_timestamp = get_meta_content_or_default('build-timestamp', new Date().toISOString());
-export const build_hash = get_meta_content_or_default('build-hash', uuid());
+export const build_timestamp = get_meta_content_or_default('build-timestamp');
+export const build_hash = get_meta_content_or_default('build-hash');
