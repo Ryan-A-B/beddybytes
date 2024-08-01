a710b7cad185c64eb8c717603503bcb9ce288b43
rballinger <rballinger@topcon.com>
2024-08-01 23:06:18 +1000

    updates paulgconlon landing page

--- marketing/src/pages/paulgconlon/index.tsx
+++ marketing/src/pages/paulgconlon/index.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import type { HeadFC } from "gatsby"
 import { StaticImage } from 'gatsby-plugin-image'
 import DefaultPageWrapper from '../../components/DefaultPageWrapper'
-import FeaturesCurrent from '../../components/FeaturesCurrent'
-import FeaturesComingSoon from '../../components/FeaturesComingSoon'
-import CallToAction from './CallToAction'
 import DemoSection from '../../components/LandingPage/DemoSection'
 import SocialProofSection from '../../components/SocialProof/Section'
 import FAQSection from '../../components/LandingPage/FAQSection'
+import Features from '../../components/Features'
+import CallToAction from './CallToAction'
 
 const CitizenOneLink: React.FunctionComponent = () => <a href="https://paulgconlon.com" target="_blank" className="link-secondary">Citizen One</a>
 
@@ -19,9 +18,9 @@ const PaulGConlon = () => (
                     <div className="container ">
                         <div className="row align-items-center">
                             <div className="col text-center">
-                                <h1 >Welcome my freedom loving friends!</h1>
+                                <h1>Special Offer</h1>
+                                <h5>Celebrate Privacy with BeddyBytes and <CitizenOneLink /></h5>
                                 <p>
-                                    As we eagerly await the release of <CitizenOneLink />, I'm excited to share with you a special offer from BeddyBytes.
                                     BeddyBytes transforms your smartphone and laptop into a private and convenient baby monitor.
                                     BeddyBytes won't send a single frame of video over the internet.
                                     We keep your data safe by never handling it in the first place.
@@ -40,51 +39,66 @@ const PaulGConlon = () => (
                 </section>
                 <section id="problem">
                     <div className="container">
-                        <h2>
-                            You want a baby monitor, not a video broadcasting service.
-                        </h2>
                         <p>
-                            Like us you are probably looking for a baby monitor that cares as much about privacy as you do.
-                            If you've read a few privacy policies you'll know that most baby monitors which connect to the cloud are creepy at best.
-                            And, even if the privacy policy checks out, data leaks and breaches happen all the time.
+                            As parents, we cherish every moment with our little ones and strive to protect their
+                            future. Yet it's hard for many to grasp that digital protection is now every bit as
+                            important as physical protection.
                         </p>
-                        <section>
-                            <h3>Privacy red flags</h3>
-                            <p>While it's possible to provide the following features while maintaining privacy, most companies don't.</p>
-                            <ul>
-                                <li>AI features: companies use your data to train their models</li>
-                                <li>Remote viewing: your video is quite likely passing through somebody else's server</li>
-                                <li>Recording: your video is probably being stored remotely</li>
-                            </ul>
-                        </section>
+                        <h6>Why's that?</h6>
+                        <p>
+                            It's a complex question that now has a simple answer. With the release of the
+                            thought-provoking book, Citizen One, fellow engineer Paul G Conlon makes an
+                            excellent case for taking your children's digital presence seriously - starting now!
+                        </p>
                     </div>
                 </section>
                 <section id="solution">
                     <div className="container">
-                        <h2>
-                            Keep your data at home using our baby monitor web app.
-                        </h2>
-                        <p>
-                            The best way to keep your data private is to never give it out in the first place.
-                            We stream nap time directly between your smartphone and laptop using your home WiFi.
-                        </p>
-                        <p>
-                            An internet connection is required to establish the connection between devices. But no video is sent over the internet.
-                        </p>
-                        <section>
-                            <h3>Features</h3>
-                            <div className="row">
-                                <div className="col-md">
-                                    <FeaturesCurrent />
-                                </div>
-                                <div className="col-md">
-                                    <FeaturesComingSoon />
-                                </div>
+                        <div className="row align-items-center">
+                            <div className="col-md">
+                                <h4>What can Citizen One do for me?</h4>
+                                <p>
+                                    Citizen One reveals the dangers of digital identification and the importance of online
+                                    anonymity. It draws chilling parallels between historical identity harvesting and
+                                    modern surveillance, emphasising how protecting personal data starts at home.
+                                </p>
+                                <h4>How can BeddyBytes help?</h4>
+                                <p>
+                                    In a world where digital privacy is increasingly compromised, BeddyBytes transforms
+                                    your smartphone and laptop into a secure baby monitor, ensuring that images never
+                                    leave your devices. With BeddyBytes, your family's digital privacy is our top priority -
+                                    because we use it for ours.
+                                </p>
+                                <p>
+                                    Ready to take control of the cameras that watch over you with an exclusive offer?
+                                </p>
+                                <p>
+                                    With Citizen One, you will understand why digital privacy matters, and with
+                                    BeddyByes you can do something about it! To celebrate the release of Citizen One,
+                                    we're excited to offer a special discount on BeddyBytes for a limited time.
+                                </p>
+                                <p>
+                                    Gifting your children a clean digital slate as they enter adulthood is maybe the most
+                                    profoundly valuable gift parents can now bequeath.
+                                    Join the privacy renaissance and do something today your children will thank you for
+                                    tomorrow.
+                                </p>
+                                <CallToAction />
                             </div>
-                        </section>
+                            <div className="d-none d-md-block col-md col-lg-5 col-xl-4">
+                                <StaticImage
+                                    src="../../images/CitizenOne.png"
+                                    alt="Citizen One"
+                                    className="img-fluid"
+                                />
+                            </div>
+                        </div>
                     </div>
                 </section>
             </main>
+            <Features backgroundClassName="bg-tertiary">
+                <h2>Features</h2>
+            </Features>
             <DemoSection />
             <SocialProofSection />
             <FAQSection />
