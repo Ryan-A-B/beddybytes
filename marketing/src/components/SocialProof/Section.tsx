7ef79bfe55ea0a4c19902bf069590e5514c545e7
rballinger <rballinger@topcon.com>
2024-08-28 09:24:56 +1000

    adds social proof to pricing page

--- marketing/src/components/SocialProof/Section.tsx
+++ marketing/src/components/SocialProof/Section.tsx
@@ -1,41 +1,39 @@
 import React from 'react'
 import './style.scss'
 
-const SocialProofSection: React.FunctionComponent = () => {
-    return (
-        <section id="social-proof" className="bg-secondary text-bg-secondary py-5 ">
-            <div className="container">
-                <h1 className="text-center mb-4">
-                    Over <span className="text-light">1,500</span> hours monitored!
-                </h1>
-                <h3 className="text-center mb-3">
-                    Here's what some of our users have to say
-                </h3>
-                <section className="testimonials">
-                    <blockquote className="blockquote">
-                        BeddyBytes is very easy to use and I love that it's flexible.
-                        I can open the parent station on my phone or laptop depending
-                        on whether I'm studying or doing housework without lugging
-                        around an extra screen. Knowing that images of our family
-                        life are completely private is very reassuring too.
-                    </blockquote>
-                    <blockquote className="blockquote">
-                        Great job! Your emphasis on security and sustainability by
-                        using existing devices is commendable.
-                    </blockquote>
-                    <blockquote className="blockquote">
-                        Love that it keeps things local for privacy.
-                    </blockquote>
-                    <blockquote className="blockquote">
-                        The girls were tidying the garage this morning and normally I
-                        miss out on all their daily activities but I had BeddyBytes
-                        running on a tablet next to me during meeting and I could see
-                        and hear them so felt more included.
-                    </blockquote>
-                </section>
-            </div>
-        </section>
-    )
-}
+const SocialProofSection: React.FunctionComponent = () => (
+    <section id="social-proof" className="bg-secondary text-bg-secondary py-5 ">
+        <div className="container">
+            <h1 className="text-center mb-4">
+                Over <span className="text-light">1,500</span> hours monitored!
+            </h1>
+            <h3 className="text-center mb-3">
+                Here's what some of our users have to say
+            </h3>
+            <section className="testimonials">
+                <blockquote className="blockquote">
+                    BeddyBytes is very easy to use and I love that it's flexible.
+                    I can open the parent station on my phone or laptop depending
+                    on whether I'm studying or doing housework without lugging
+                    around an extra screen. Knowing that images of our family
+                    life are completely private is very reassuring too.
+                </blockquote>
+                <blockquote className="blockquote">
+                    Great job! Your emphasis on security and sustainability by
+                    using existing devices is commendable.
+                </blockquote>
+                <blockquote className="blockquote">
+                    Love that it keeps things local for privacy.
+                </blockquote>
+                <blockquote className="blockquote">
+                    The girls were tidying the garage this morning and normally I
+                    miss out on all their daily activities but I had BeddyBytes
+                    running on a tablet next to me during meeting and I could see
+                    and hear them so felt more included.
+                </blockquote>
+            </section>
+        </div>
+    </section>
+)
 
 export default SocialProofSection
--- marketing/src/pages/pricing.tsx
+++ marketing/src/pages/pricing.tsx
@@ -5,19 +5,23 @@ import DefaultPageWrapper from '../components/DefaultPageWrapper'
 import Pricing from '../components/Pricing'
 import TrialPeriod from '../components/Pricing/TrialPeriod'
 import RedirectToPaymentProcessor from '../components/Pricing/RedirectToPaymentProcessor'
+import SocialProofSection from '../components/SocialProof/Section'
 
 const maxWidth = 760
 
 const PricingPage: React.FunctionComponent<PageProps> = () => {
     return (
         <DefaultPageWrapper without_call_to_action_section>
-            <main className="container" style={{ maxWidth }}>
-                <h1 className="text-center mt-3 fs-5">Pricing</h1>
-                <h4 className="text-center fs-2">No Subscription</h4>
-                <RedirectToPaymentProcessor />
-                <Pricing />
-                <TrialPeriod />
+            <main className="bg-primary text-bg-primary py-5">
+                <div className="container" style={{ maxWidth }}>
+                    <h1 className="text-center mt-3 fs-5">Pricing</h1>
+                    <h4 className="text-center fs-2">No Subscription</h4>
+                    <RedirectToPaymentProcessor />
+                    <Pricing />
+                    <TrialPeriod />
+                </div>
             </main>
+            <SocialProofSection />
         </DefaultPageWrapper>
     )
 }
