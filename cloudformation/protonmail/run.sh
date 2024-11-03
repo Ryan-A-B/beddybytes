dc6ac2dafa15a7526149386b4f7edab3d1ec43cb
rballinger <rballinger@topcon.com>
2024-11-03 11:06:35 +1000

    adds proton mail DNS records

---
+++ cloudformation/protonmail/run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+. init.sh
+set -ex
+
+usage="Usage: $0 <create|update|delete>"
+
+case $1 in
+    create|update|delete)
+        action=$1
+        ;;
+    *)
+        echo $usage
+        exit 1
+        ;;
+esac
+
+region=us-east-1
+stack_name="beddybytes-protonmail"
+
+case $action in
+    create|update)
+        parameters_file="parameters.json"
+        aws cloudformation $action-stack \
+            --region $region \
+            --stack-name $stack_name \
+            --template-body file://cloudformation.yml \
+            --parameters file://$parameters_file
+        ;;
+    delete)
+        aws cloudformation delete-stack \
+            --region $region \
+            --stack-name $stack_name
+        ;;
+    *)
+        echo "Unexpected action: $action"
+        exit 1
+        ;;
+esac
+
+aws cloudformation wait stack-$action-complete \
+    --region $region \
+    --stack-name $stack_name
