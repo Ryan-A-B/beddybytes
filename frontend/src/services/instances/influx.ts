08eec859b0a64eb0ca5a389e900dc96d796581e0
rballinger <rballinger@topcon.com>
2025-01-04 14:55:19 +1000

    updates frontend to use new influxdb

--- frontend/src/services/instances/influx.ts
+++ frontend/src/services/instances/influx.ts
@@ -6,8 +6,8 @@ const getClient = (): InfluxDB => {
         token: '89X2G06CruyZ1lPpMAOhd_VzjuXiNhkQ1wOy27U_gfkIqXjqmYKK32BSVvgIDSyGFclijF2b1HVKBVN6k6XeeA==',
     })
     return new InfluxDB({
-        url: 'https://influx.beddybytes.com',
-        token: 'QWPp6p0hcgPozmUDKfb6dgEpbcmugF98nkSqtCsZfDxt8iHZJ6Ky-IDaZwsCKC7wUPIvKagoF5eExaECks9BBw==',
+        url: 'https://influxdb.beddybytes.com',
+        token: '7YVbDqZErhr8XFS2dCMFR-hD1T9OmblkeKMMB2rlwNaKaV7k3XkqiQNUUv9XyKu7HH_s-qK9tPjMyEicubpx1w==',
     })
 }
 
