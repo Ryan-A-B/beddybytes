ddee34b5ee972efc019dccff1f49a43c9529eb7e
rballinger <rballinger@topcon.com>
2025-05-26 09:26:20 +1000

    updates backend build script to use AWS code-build

--- scripts/backend/build.sh
+++ scripts/backend/build.sh
@@ -2,9 +2,29 @@
 
 source scripts/backend/init.sh
 
-docker build \
-    --file scripts/backend/Dockerfile \
-    --tag beddybytes-backend-api \
-    .
+case $1 in
+    qa|prod)
+        env=$1
+        ;;
+    *)
+        echo "Usage: $0 <qa|prod>"
+        exit 1
+        ;;
+esac
 
-docker tag beddybytes-backend-api $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/beddybytes-backend-api:latest
+if [ "$env" = "prod" ]; then
+    echo "Production builds are not supported yet."
+    exit 1
+fi
+
+if [ -n "$2" ]; then
+    branch=$2
+else
+    branch=$env
+fi
+
+aws codebuild start-build \
+  --project-name $BUILD_PROJECT_NAME \
+  --environment-variables-override name=DOCKER_IMAGE_TAG,value=$env,type=PLAINTEXT \
+  --source-version $branch \
+  --region $AWS_REGION
--- scripts/backend/push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-source scripts/backend/init.sh
-
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
-
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/beddybytes-backend-api:latest
