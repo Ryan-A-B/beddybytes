5fb9c42cbd04a21d1c9e1164f930bd860f1aee09
rballinger <rballinger@topcon.com>
2025-06-05 07:50:00 +1000

    clears errors on no_account. makes usePlay log messages Error rather than Critical

--- frontend/src/services/instances/error_service.ts
+++ frontend/src/services/instances/error_service.ts
@@ -1,8 +1,16 @@
 import ErrorService from "../ErrorService";
 import logging_service from "./logging_service";
+import authorization_service from "./authorization_service";
 
 const error_service = new ErrorService({
     logging_service,
 });
 
 export default error_service;
+
+authorization_service.addEventListener('statechange', (event) => {
+    const state = authorization_service.get_state();
+    if (state.state === 'no_account') {
+        error_service.clear_errors();
+    }
+});
