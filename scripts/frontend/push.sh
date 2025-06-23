a1e7340c0fcf32eba4c57834f671d6428de65b96
rballinger <rballinger@topcon.com>
2025-06-23 13:57:19 +1000

    adds alternative formats for audio

--- scripts/frontend/push.sh
+++ scripts/frontend/push.sh
@@ -22,6 +22,26 @@ fi
 region=us-east-1
 bucket="beddybytes-$env-frontend-bucket"
 
-aws s3 sync --region $region frontend/build s3://$bucket
+# Cache-Control
+# - static files: immutable
+# - index.html: don't cache
+# - everything else: cache for 1 day
+
+# the service worker does network then cache for .html files
+# cache then network for everything else
+
+aws s3 sync --region $region --delete \
+    --cache-control "max-age=31536000, immutable" \
+    "frontend/build/static" "s3://$bucket/static"
+
+aws s3 sync --region $region --delete \
+    --cache-control "max-age=86400" \
+    --exclude "static/*" \
+    --exclude "index.html" \
+    "frontend/build" "s3://$bucket/"
+
+aws s3 cp --region $region \
+    --cache-control "no-cache" \
+    "frontend/build/index.html" "s3://$bucket/index.html"
 
 aws cloudfront create-invalidation --region $region --distribution-id $DISTRIBUTION_ID --paths "/*"
