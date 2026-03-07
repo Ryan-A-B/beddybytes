5fd7cb15648482b94d6448fc1a912eb93887092d
rballinger <rballinger@topcon.com>
2026-03-07 13:38:45 +1000

    adds tiny analytics to marketing page and local stack

--- marketing/gatsby-ssr.tsx
+++ marketing/gatsby-ssr.tsx
@@ -2,13 +2,17 @@ import React from "react"
 import { GatsbySSR } from "gatsby"
 
 export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }) => {
-    if (process.env.NODE_ENV !== 'production') return;
-    // setHeadComponents([
-    //     <script
-    //         key="plausible"
-    //         data-domain="beddybytes.com"
-    //         src="https://plausible.io/js/script.outbound-links.js"
-    //         defer
-    //     />
-    // ])
+    const endpoint = process.env.TINYANALYTICS_ENDPOINT || "https://analytics.beddybytes.com"
+    const writeToken = process.env.TINYANALYTICS_WRITE_TOKEN
+
+    setHeadComponents([
+        <script
+            key="tinyanalytics-tracker"
+            defer
+            src="/js/tinyanalytics-v0.autotrack.js"
+            data-endpoint={endpoint}
+            data-include-referrer="true"
+            {...(writeToken ? { "data-write-token": writeToken } : {})}
+        />,
+    ])
 }
--- scripts/marketing/build.sh
+++ scripts/marketing/build.sh
@@ -6,4 +6,6 @@ workdir=$(pwd)/marketing
 docker run --rm -it \
     -v $workdir:$workdir \
     -w $workdir \
+    -e TINYANALYTICS_ENDPOINT=${TINYANALYTICS_ENDPOINT:-https://analytics.beddybytes.com} \
+    -e TINYANALYTICS_WRITE_TOKEN=${TINYANALYTICS_WRITE_TOKEN:-} \
     node:18 npm run build
