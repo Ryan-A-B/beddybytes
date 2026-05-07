a6f7f1d393e868982c448c7e9176505e30141485
ryan <2224076+Ryan-A-B@users.noreply.github.com>
2026-05-07 21:11:23 +1000

    ..

--- integration_tests/test.sh
+++ integration_tests/test.sh
@@ -3,18 +3,20 @@ set -ex
 
 ./ensure_venv.sh
 
-# python -m unittest discover -s src -v/
+python -m unittest discover -s src -v
 
 cd src
 
-# python -m unittest tests.test_session
-# python -m unittest tests.test_recording
-# python -m unittest tests.test_two_baby_stations
+# python -m unittest -v tests.test_session
+# python -m unittest -v tests.test_recording
+# python -m unittest -v tests.test_two_baby_stations
+# python -m unittest -v tests.test_backend_http_api
+# python -m unittest -v tests.test_backend_mqtt
 
 # python -m unittest tests.test_session.TestSession.test_audio_session
 # python -m unittest tests.test_session.TestSession.test_video_session
 # python -m unittest tests.test_session.TestSession.test_restart_and_reconnect_session
-python -m unittest tests.test_session.TestSession.test_parent_station_recovers_baby_station_list_after_mosquitto_restarts
+# python -m unittest tests.test_session.TestSession.test_parent_station_recovers_baby_station_list_after_mosquitto_restarts
 
 # python -m unittest tests.test_two_baby_stations.TestTwoBabyStations.test_switching_between_baby_stations
 # python -m unittest tests.test_two_baby_stations.TestTwoBabyStations.test_parent_station_second
